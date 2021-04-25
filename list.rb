c20b81054a110c1a38fca72fa9a258abddf6fc37
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-04-24 18:53:46 -0700

    add item works

--- list.rb
+++ list.rb
@@ -1,3 +1,18 @@
+require './item'
+
 class List
+attr_accessor :label
+
+def initialize(label)
+    @label = label
+    @items = []
+end
+
+def add_item(title, deadline, description="")
+    return false if !Item.valid_date?(deadline)
+    new_item = Item.new(title, deadline, description)
+    @items << new_item
+    true
+end
 
 end
