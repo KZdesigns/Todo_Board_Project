f6b5751c76650bd7dcba3fed1e63ea88170410f1
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-04-24 22:25:24 -0700

    updated list class, added swap, index, and priority

--- list.rb
+++ list.rb
@@ -2,6 +2,7 @@ require './item'
 
 class List
 attr_accessor :label
+attr_writer :items
 
 def initialize(label)
     @label = label
@@ -14,5 +15,34 @@ def add_item(title, deadline, description="")
     @items << new_item
     true
 end
+
+def size
+    @items.length  
+end
+
+def valid_index?(index)
+    return true if (index <= (@items.size - 1)) && index >= 0
+    false
+end
+
+def swap(index_1, index_2)
+    return false if !self.valid_index?(index_1) || !self.valid_index?(index_2)
+    @items[index_1], @items[index_2] = @items[index_2], @items[index_1]
+    true
+end
+
+def [](index)
+    return nil if !self.valid_index?(index)
+    @items[index]
+end
+
+def priority
+    @items.first
+end
+
+def print
+    
+end
+
 
 end
